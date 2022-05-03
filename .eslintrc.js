130a250a24dd1ef715a4bea49502a2d1c5ddad8e
Artur Kutusenko <kutusenko@gmail.com>
2022-05-03 16:30:43 +0300

    adds eslint options

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
@@ -19,6 +20,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-empty-function': 'off'
+    '@typescript-eslint/no-empty-function': 'off',
+    'no-console': 2,
   },
 };
