47b0f58c6f0abc37803f0539a8767ac208fe1d47
Artur Kutusenko <kutusenko@gmail.com>
2022-05-24 22:11:23 +0300

    fixed error

--- src/modules/user/contact-info/contacts.controller.ts
+++ src/modules/user/contact-info/contacts.controller.ts
@@ -1,44 +1,40 @@
-import {
-    Body,
-    Controller,
-    Get,
-    Post,
-    Req
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 
-import { ContactsService } from './contacts.service';
-import { UserEntity } from "../user.entity";
+import { UserEntity } from '../user.entity';
 import { ContactsDTO } from '../dto/contactsDTO';
 import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Contact info')
 @Controller('user/contacts')
 export class ContactsController {
-    constructor(private readonly ContactsService: ContactsService) { }
+  constructor(private readonly ContactsService: ContactsService) {}
 
-    @Get()
-    findOne(@Req() req: Request): Promise<UserEntity> {
-        try {
-            return this.ContactsService.findOne(req)
-        }
-        catch { throw new Error() }
-
+  @Get()
+  findOne(@Req() req: Request): Promise<UserEntity> {
+    try {
+      return this.ContactsService.findOne(req);
+    } catch {
+      throw new Error();
     }
+  }
 
-    @Post()
-    update(@Body() data: ContactsDTO, @Req() req: Request): string {
-        try {
-            this.ContactsService.update(req, data)
-            return 'Contact info succefully updated!'
-        } catch {
-            throw new Error('Something went wrong')
-        }
+  @Post()
+  update(@Body() data: ContactsDTO, @Req() req: Request): string {
+    try {
+      this.ContactsService.update(req, data);
+      return 'Contact info succefully updated!';
+    } catch {
+      throw new Error('Something went wrong');
     }
+  }
 
-    @Get('/all')
-    findAll(): Promise<UserEntity[]> {
-        try { return this.ContactsService.find() }
-        catch { throw new Error() }
+  @Get('/all')
+  findAll(): Promise<UserEntity[]> {
+    try {
+      return this.ContactsService.find();
+    } catch {
+      throw new Error();
     }
+  }
 }
